3350d34f184fc8b851d84f06488e030bbd480f35
Scott Long <longstoryscott@gmail.com>
2022-03-14 14:17:10 -0500

    undo custom firmwar upload and configuration embedding

--- Marlin/config_adv_lsm.h
+++ Marlin/config_adv_lsm.h
@@ -694,9 +694,9 @@
 #endif
 #if EITHER(CASE_LIGHT_USE_NEOPIXEL, CASE_LIGHT_USE_RGB_LED)
 #define CASE_LIGHT_DEFAULT_COLOR \
-  {                              \
-    255, 255, 255, 255           \
-  } // { Red, Green, Blue, White }
+    {                            \
+        255, 255, 255, 255       \
+    } // { Red, Green, Blue, White }
 #endif
 #endif
 
@@ -850,13 +850,13 @@
 //#define SENSORLESS_BACKOFF_MM  { 2, 2, 0 }  // (mm) Backoff from endstops before sensorless homing
 
 #define HOMING_BUMP_MM \
-  {                    \
-    5, 5, 2            \
-  } // (mm) Backoff from endstops after first bump
+    {                  \
+        5, 5, 2        \
+    } // (mm) Backoff from endstops after first bump
 #define HOMING_BUMP_DIVISOR \
-  {                         \
-    2, 2, 4                 \
-  } // Re-Bump Speed Divisor (Divides the Homing Feedrate)
+    {                       \
+        2, 2, 4             \
+    } // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
 //#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (mm) Backoff from endstops after homing
 
@@ -1005,10 +1005,10 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
 // Define positions for probe points.
-#define TRAMMING_POINT_XY                        \
-  {                                              \
-    {20, 20}, {180, 20}, {180, 180}, { 20, 180 } \
-  }
+#define TRAMMING_POINT_XY                            \
+    {                                                \
+        {20, 20}, {180, 20}, {180, 180}, { 20, 180 } \
+    }
 
 // Define position names for probe points.
 #define TRAMMING_POINT_NAME_1 "Front-Left"
@@ -1035,10 +1035,10 @@
 
 // @section motion
 
-#define AXIS_RELATIVE_MODES    \
-  {                            \
-    false, false, false, false \
-  }
+#define AXIS_RELATIVE_MODES        \
+    {                              \
+        false, false, false, false \
+    }
 
 // Add a Duplicate option for well-separated conjoined nozzles
 //#define MULTI_NOZZLE_DUPLICATION
@@ -1105,9 +1105,9 @@
 // Define values for backlash distance and correction.
 // If BACKLASH_GCODE is enabled these values are the defaults.
 #define BACKLASH_DISTANCE_MM \
-  {                          \
-    0, 0, 0                  \
-  }                             // (mm) One value for each linear axis
+    {                        \
+        0, 0, 0              \
+    }                           // (mm) One value for each linear axis
 #define BACKLASH_CORRECTION 0.0 // 0.0 = no correction; 1.0 = full correction
 
 // Add steps for motor direction changes on CORE kinematics
@@ -1169,13 +1169,13 @@
 
 // The true location and dimension the cube/bolt/washer on the bed.
 #define CALIBRATION_OBJECT_CENTER \
-  {                               \
-    264.0, -22.0, -2.0            \
-  } // mm
+    {                             \
+        264.0, -22.0, -2.0        \
+    } // mm
 #define CALIBRATION_OBJECT_DIMENSIONS \
-  {                                   \
-    10.0, 10.0, 10.0                  \
-  } // mm
+    {                                 \
+        10.0, 10.0, 10.0              \
+    } // mm
 
 // Comment out any sides which are unreachable by the probe. For best
 // auto-calibration results, all sides must be reachable.
@@ -1224,10 +1224,10 @@
 //#define MICROSTEP32 HIGH,LOW,HIGH
 
 // Microstep settings (Requires a board with pins named X_MS1, X_MS2, etc.)
-#define MICROSTEP_MODES    \
-  {                        \
-    16, 16, 16, 16, 16, 16 \
-  } // [1,2,4,8,16]
+#define MICROSTEP_MODES        \
+    {                          \
+        16, 16, 16, 16, 16, 16 \
+    } // [1,2,4,8,16]
 
 /**
  *  @section  stepper motor current
@@ -1263,10 +1263,10 @@
 
 // Actual motor currents in Amps. The number of entries must match DIGIPOT_I2C_NUM_CHANNELS.
 // These correspond to the physical drivers, so be mindful if the order is changed.
-#define DIGIPOT_I2C_MOTOR_CURRENTS         \
-  {                                        \
-    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 \
-  } // AZTEEG_X3_PRO
+#define DIGIPOT_I2C_MOTOR_CURRENTS             \
+    {                                          \
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 \
+    } // AZTEEG_X3_PRO
 
 //#define DIGIPOT_USE_RAW_VALUES    // Use DIGIPOT_MOTOR_CURRENT raw wiper values (instead of A4988 motor currents)
 
@@ -1291,10 +1291,10 @@
 // @section lcd
 
 #if HAS_MANUAL_MOVE_MENU
-#define MANUAL_FEEDRATE              \
-  {                                  \
-    50 * 60, 50 * 60, 4 * 60, 2 * 60 \
-  }                            // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
+#define MANUAL_FEEDRATE                  \
+    {                                    \
+        50 * 60, 50 * 60, 4 * 60, 2 * 60 \
+    }                          // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
 #define FINE_MANUAL_MOVE 0.025 // (mm) Smallest manual move (< 0.1mm) applying to Z on most machines
 #if IS_ULTIPANEL
 #define MANUAL_E_MOVES_RELATIVE // Display extruder move distance rather than "position"
@@ -1351,9 +1351,9 @@
 #define XATC_MAX_POINTS 3        // Number of points to probe in the wizard
 #define XATC_Y_POSITION Y_CENTER // (mm) Y position to probe
 #define XATC_Z_OFFSETS \
-  {                    \
-    0, 0, 0            \
-  } // Z offsets for X axis sample points
+    {                  \
+        0, 0, 0        \
+    } // Z offsets for X axis sample points
 #endif
 #endif
 
@@ -1650,14 +1650,14 @@
  * Use 'M503 C' to write the settings out to the SD Card as 'mc.zip'.
  * See docs/ConfigEmbedding.md for details on how to use 'mc-apply.py'.
  */
-// #define CONFIGURATION_EMBEDDING
+#define CONFIGURATION_EMBEDDING
 
 // Add an optimized binary file transfer mode, initiated with 'M28 B1'
 #define BINARY_FILE_TRANSFER
 
 #if ENABLED(BINARY_FILE_TRANSFER)
 // Include extra facilities (e.g., 'M20 F') supporting firmware upload via BINARY_FILE_TRANSFER
-// #define CUSTOM_FIRMWARE_UPLOAD
+#define CUSTOM_FIRMWARE_UPLOAD
 #endif
 
 /**
@@ -2144,9 +2144,9 @@
 #define PTC_PROBE_RES 5    // (°C)
 #define PTC_PROBE_COUNT 10
 #define PTC_PROBE_ZOFFS \
-  {                     \
-    0                   \
-  } // (µm) Z adjustments per sample
+    {                   \
+        0               \
+    } // (µm) Z adjustments per sample
 #endif
 
 #if ENABLED(PTC_BED)
@@ -2155,9 +2155,9 @@
 #define PTC_BED_RES 5    // (°C)
 #define PTC_BED_COUNT 10
 #define PTC_BED_ZOFFS \
-  {                   \
-    0                 \
-  } // (µm) Z adjustments per sample
+    {                 \
+        0             \
+    } // (µm) Z adjustments per sample
 #endif
 
 #if ENABLED(PTC_HOTEND)
@@ -2166,25 +2166,25 @@
 #define PTC_HOTEND_RES 5     // (°C)
 #define PTC_HOTEND_COUNT 20
 #define PTC_HOTEND_ZOFFS \
-  {                      \
-    0                    \
-  } // (µm) Z adjustments per sample
+    {                    \
+        0                \
+    } // (µm) Z adjustments per sample
 #endif
 
 // G76 options
 #if BOTH(PTC_PROBE, PTC_BED)
 // Park position to wait for probe cooldown
 #define PTC_PARK_POS \
-  {                  \
-    0, 0, 100        \
-  }
+    {                \
+        0, 0, 100    \
+    }
 
 // Probe position to probe and wait for probe to reach target temperature
 //#define PTC_PROBE_POS  { 12.0f, 7.3f } // Example: MK52 magnetic heatbed
 #define PTC_PROBE_POS \
-  {                   \
-    90, 100           \
-  }
+    {                 \
+        90, 100       \
+    }
 
 // The temperature the probe should be at while taking measurements during
 // bed temperature calibration.
@@ -2534,10 +2534,10 @@
  */
 //#define TOOLCHANGE_PARK
 #if ENABLED(TOOLCHANGE_PARK)
-#define TOOLCHANGE_PARK_XY         \
-  {                                \
-    X_MIN_POS + 10, Y_MIN_POS + 10 \
-  }
+#define TOOLCHANGE_PARK_XY             \
+    {                                  \
+        X_MIN_POS + 10, Y_MIN_POS + 10 \
+    }
 #define TOOLCHANGE_PARK_XY_FEEDRATE 6000 // (mm/min)
 //#define TOOLCHANGE_PARK_X_ONLY          // X axis only move
 //#define TOOLCHANGE_PARK_Y_ONLY          // Y axis only move
@@ -3188,8 +3188,8 @@
  * }
  */
 #define TMC_ADV() \
-  {               \
-  }
+    {             \
+    }
 
 #endif // HAS_TRINAMIC_CONFIG
 
@@ -4137,18 +4137,18 @@
 //#define INVERT_JOY_Z  // Enable if Z direction is reversed
 
 // Use M119 with JOYSTICK_DEBUG to find reasonable values after connecting:
-#define JOY_X_LIMITS                    \
-  {                                     \
-    5600, 8190 - 100, 8190 + 100, 10800 \
-  } // min, deadzone start, deadzone end, max
-#define JOY_Y_LIMITS                    \
-  {                                     \
-    5600, 8250 - 100, 8250 + 100, 11000 \
-  }
-#define JOY_Z_LIMITS                    \
-  {                                     \
-    4800, 8080 - 100, 8080 + 100, 11550 \
-  }
+#define JOY_X_LIMITS                        \
+    {                                       \
+        5600, 8190 - 100, 8190 + 100, 10800 \
+    } // min, deadzone start, deadzone end, max
+#define JOY_Y_LIMITS                        \
+    {                                       \
+        5600, 8250 - 100, 8250 + 100, 11000 \
+    }
+#define JOY_Z_LIMITS                        \
+    {                                       \
+        4800, 8080 - 100, 8080 + 100, 11550 \
+    }
 //#define JOYSTICK_DEBUG
 #endif
 
@@ -4230,10 +4230,10 @@
  * Ethernet. Use M552 to enable and set the IP address.
  */
 #if HAS_ETHERNET
-#define MAC_ADDRESS                    \
-  {                                    \
-    0xDE, 0xAD, 0xBE, 0xEF, 0xF0, 0x0D \
-  } // A MAC address unique to your network
+#define MAC_ADDRESS                        \
+    {                                      \
+        0xDE, 0xAD, 0xBE, 0xEF, 0xF0, 0x0D \
+    } // A MAC address unique to your network
 #endif
 
 /**
@@ -4296,28 +4296,28 @@
 // This is for Průša MK3-style extruders. Customize for your hardware.
 #define MMU2_FILAMENTCHANGE_EJECT_FEED 80.0
 #define MMU2_LOAD_TO_NOZZLE_SEQUENCE \
-  {7.2, 1145},                       \
-      {14.4, 871},                   \
-      {36.0, 1393},                  \
-      {14.4, 871},                   \
-  {                                  \
-    50.0, 198                        \
-  }
+    {7.2, 1145},                     \
+        {14.4, 871},                 \
+        {36.0, 1393},                \
+        {14.4, 871},                 \
+    {                                \
+        50.0, 198                    \
+    }
 
 #define MMU2_RAMMING_SEQUENCE \
-  {1.0, 1000},                \
-      {1.0, 1500},            \
-      {2.0, 2000},            \
-      {1.5, 3000},            \
-      {2.5, 4000},            \
-      {-15.0, 5000},          \
-      {-14.0, 1200},          \
-      {-6.0, 600},            \
-      {10.0, 700},            \
-      {-10.0, 400},           \
-  {                           \
-    -50.0, 2000               \
-  }
+    {1.0, 1000},              \
+        {1.0, 1500},          \
+        {2.0, 2000},          \
+        {1.5, 3000},          \
+        {2.5, 4000},          \
+        {-15.0, 5000},        \
+        {-14.0, 1200},        \
+        {-6.0, 600},          \
+        {10.0, 700},          \
+        {-10.0, 400},         \
+    {                         \
+        -50.0, 2000           \
+    }
 #endif
 
 /**
@@ -4329,21 +4329,21 @@
 #define MMU2_C0_RETRY 5 // Number of retries (total time = timeout*retries)
 
 #define MMU2_CAN_LOAD_FEEDRATE 800 // (mm/min)
-#define MMU2_CAN_LOAD_SEQUENCE        \
-  {0.1, MMU2_CAN_LOAD_FEEDRATE},      \
-      {60.0, MMU2_CAN_LOAD_FEEDRATE}, \
-  {                                   \
-    -52.0, MMU2_CAN_LOAD_FEEDRATE     \
-  }
+#define MMU2_CAN_LOAD_SEQUENCE          \
+    {0.1, MMU2_CAN_LOAD_FEEDRATE},      \
+        {60.0, MMU2_CAN_LOAD_FEEDRATE}, \
+    {                                   \
+        -52.0, MMU2_CAN_LOAD_FEEDRATE   \
+    }
 
 #define MMU2_CAN_LOAD_RETRACT 6.0   // (mm) Keep under the distance between Load Sequence values
 #define MMU2_CAN_LOAD_DEVIATION 0.8 // (mm) Acceptable deviation
 
 #define MMU2_CAN_LOAD_INCREMENT 0.2 // (mm) To reuse within MMU2 module
-#define MMU2_CAN_LOAD_INCREMENT_SEQUENCE             \
-  {                                                  \
-    -MMU2_CAN_LOAD_INCREMENT, MMU2_CAN_LOAD_FEEDRATE \
-  }
+#define MMU2_CAN_LOAD_INCREMENT_SEQUENCE                 \
+    {                                                    \
+        -MMU2_CAN_LOAD_INCREMENT, MMU2_CAN_LOAD_FEEDRATE \
+    }
 
 #else
 
